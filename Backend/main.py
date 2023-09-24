50e0ec734d8c7d1b4a980ac25bd4de13d8e232e6
Aaditya Naik <aadityasnaik007@gmail.com>
2023-09-23 19:01:17 -0700

    added backend server code with FastAPI

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,29 @@
-print("Main Backend File")
+import uvicorn
+from fastapi import FastAPI,File,UploadFile,status,Response
+import pandas as pd
+import io 
+
+app = FastAPI()
+
+@app.get('/check_status',status_code=status.HTTP_201_CREATED)
+def root(response:Response):
+    try:
+        return "Article prediction is in Progress!!"
+    except Exception as ex:
+        print(ex)
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"status":400,"message":str(ex)}
+    
+@app.post('/get_prediction_for_csv',status_code=status.HTTP_201_CREATED)
+def import_csv(file: UploadFile = File(...)):
+    try:
+        dataframe = pd.read_csv(io.StringIO(file.file.read().decode('utf-8')), delimiter=',')
+        print(dataframe.head())
+        return {"status":status.HTTP_201_CREATED,"message":"CSV received!"}
+    except Exception as ex:
+        print(ex)
+        return {"status":status.HTTP_400_BAD_REQUEST,"message":str(ex)}
+    
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
