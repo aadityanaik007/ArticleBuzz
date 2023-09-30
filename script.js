0f737b7b50fb676aedb49320c7a574d5536987b9
SumitRodrigues <65523336+SumitRodrigues@users.noreply.github.com>
2023-09-30 10:55:24 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -11,20 +11,20 @@ fileInput.onchange = ({target})=>{
   let file = target.files[0];
   if(file){
     let fileName = file.name;
-    if(fileName.length >= 12){
-      let splitName = fileName.split('.');
-      fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
-    }
+    // if(fileName.length >= 12){
+    //   let splitName = fileName.split('.');
+    //   fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
+    // }
     uploadFile(fileName);
   }
 }
 
 function uploadFile(name){
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "php/upload.php");
+  let xhr = new XMLHttpRequest(); //creating new XML object (AJAX)
+  xhr.open("POST", "php/upload.php"); //sending POST request to specifies URL/File
   xhr.upload.addEventListener("progress", ({loaded, total}) =>{
-    let fileLoaded = Math.floor((loaded / total) * 100);
-    let fileTotal = Math.floor(total / 1000);
+    let fileLoaded = Math.floor((loaded / total) * 100); //getting percentage of loaded filesize
+    let fileTotal = Math.floor(total / 1000); //getting filesize in KB from bytes
     let fileSize;
     (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
     let progressHTML = `<li class="row">
@@ -57,6 +57,6 @@ function uploadFile(name){
       uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
     }
   });
-  let data = new FormData(form);
-  xhr.send(data);
+  let formData = new FormData(form);
+  xhr.send(formData);
 }
