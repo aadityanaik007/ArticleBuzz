ae943cf6e0cc2a06183ed250700916179f765f45
SumitRodrigues <sumitrod11@gmail.com>
2023-12-29 20:12:58 -0800

    First commit

--- Backend/main.py
+++ Backend/main.py
@@ -7,7 +7,8 @@ import random
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+import requests
+import json
 app = FastAPI()
 origins = [
     'http://127.0.0.1:5500',
@@ -29,17 +30,6 @@ templates = Jinja2Templates(directory="../Frontend")
 @app.get("/", response_class=HTMLResponse)
 async def homepage(request: Request):
     return templates.TemplateResponse("home.html",{"request":request,"key":"value"})
-
-# @app.get('/',status_code=status.HTTP_201_CREATED)
-# def homepage(response:Response):
-#     try:
-#         return {"message":"works!!!!"}
-#     except Exception as ex:
-#         print(ex)
-#         response.status_code = status.HTTP_400_BAD_REQUEST
-#         return {"status":400,"message":str(ex)}
-    
-
         
 @app.get('/check_status',status_code=status.HTTP_201_CREATED)
 def root(response:Response):
@@ -50,16 +40,34 @@ def root(response:Response):
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"status":400,"message":str(ex)}
     
+# Calling the API Gateway endpoint 
+def gateway_api(json_data:dict):
+    try:
+        # API Gateway endpoint
+        url = "https://nj7tqz9049.execute-api.us-west-1.amazonaws.com/Dev/api_gate_res"
+
+        payload = json.dumps({
+        "data":json_data
+        })
+        headers = {
+        'Content-Type': 'application/json'
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        response = json.loads(response.text)
+        return response
+    except Exception as ex:
+        raise Exception(str(ex))
+    
 @app.post('/get_prediction_for_csv',status_code=status.HTTP_201_CREATED)
 def import_csv(file: UploadFile = File(...)):
     try:
         dataframe = pd.read_csv(io.StringIO(file.file.read().decode('utf-8')), delimiter=',')
         top_ten_df = [i.split('/')[-2] for i in dataframe['url'].head(10).tolist()]
+        response = gateway_api(data)
         return {"status":status.HTTP_201_CREATED,"model":\
-                [{"xg_boost":{"MAE":2982.32,"RMS":8957.45}},
-                {"some_other_model":{"MAE":3234.32,"RMS":1243.45}},
-                {"some_different_model":{"MAE":3452.32,"RMS":98.45}}
-                ],"data":[{"number of shares":random.randint(1, 100),"article name":i} for i in top_ten_df]}
+                [{"xg_boost":{"MAE":response['xg_boost']['MAE'],"RMS":response['xg_boost']['RMS']}},
+                {"gradient_boosting":{"MAE":response['gradient_boosting']['RMS'],"RMS":response['gradient_boosting']['RMS']}}
+                ],"data":response}
     except Exception as ex:
         print(ex)
         return {"status":status.HTTP_400_BAD_REQUEST,"message":str(ex)}
